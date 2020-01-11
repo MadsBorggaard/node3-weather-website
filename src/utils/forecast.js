524c618ce9db8b6b3f20c31e6180adbc050f7815
Mads Borggaard <mads.borggaard@outlook.dk>
2020-01-11 23:53:34 +0100

    Changed forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,18 @@
 const request = require('request')
 
 const forecast = (longitude, latitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/27247e73c04d957a8cba3cff4917c4e5/' + longitude + ',' + latitude + '?units=si&lang=da'
+    const url = 'https://api.darksky.net/forecast/27247e73c04d957a8cba3cff4917c4e5/' + longitude + ',' + latitude + '?units=si'
     request({ url, json: true }, (error, { body }) => {
         const { currently } = body
         const { temperature, precipProbability } = currently
         const summary = body.daily.data[0].summary
+        const { temperatureHigh, temperatureLow } = body.daily.data[0]
         if (error) {
             callback(error, undefined)
         } else {
-            callback(undefined, summary + ' The temp is ' + temperature + ' and there is ' + precipProbability + '% chance for rain')
+            console.log(body.daily);
+
+            callback(undefined, summary + ' The temp is between ' + temperatureLow + ' and ' + temperatureHigh + ' and there is ' + precipProbability + '% chance for rain')
         }
     })
 }
